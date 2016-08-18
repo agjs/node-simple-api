74dd34c2ea2ed27a92aebf7ff45fb37da32c78bb
Aleksandar Grbic <agrbicpp@gmail.com>
2016-08-18 17:00:33 +0200

    added links to libraries that I want you to read and check out

--- server/config/middleware.express.js
+++ server/config/middleware.express.js
@@ -2,19 +2,19 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var passport = require('passport');
+var passport = require('passport'); /* http://passportjs.org/docs/configure */
 
 module.exports = function(app) {
 
     require('./passport/local.passport_strategy');
 
-    app.use(cookieParser());
+    app.use(cookieParser()); /*https://github.com/expressjs/cookie-parser */
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-    
+
     app.use(bodyParser.json());
-    app.use(session({
+    app.use(session({       /* https://github.com/expressjs/session */
         secret: 'dylan the dunne',
         resave: false,
         saveUninitialized: false
@@ -26,3 +26,5 @@ module.exports = function(app) {
     require('./passport/sessions.passport')();
 
 }
+
+
